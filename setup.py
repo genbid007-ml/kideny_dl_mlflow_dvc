a69649d72569437a2dc754805c3cb4ce0d2987b4
genbid007-ml <genbid007.ml@gmail.com>
2023-10-06 16:22:52 -0400

    setup done

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "kideny_dl_mlflow_dvc" #repo nmae at ur github repository
+AUTHOR_USER_NAME = "genbid007-ml" #github user name
+SRC_REPO = "cnnClassifier" #src folder repo 'src/cnnClassifier'
+AUTHOR_EMAIL = "genbid007.ml@gmail.com" #github email id
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for CNN app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
